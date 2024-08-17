2ba09dfca63437a1660379fe5edda7abc819ba86
6iruk <biruktarega@gmail.com>
2024-08-17 17:20:40 +0300

    Config update

--- REST_API/settings.py
+++ REST_API/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nf-r(-%qwjb7fm$&sf%=*t9i+f6h!d$o*%$pb%cj0k!fcqk1_l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
